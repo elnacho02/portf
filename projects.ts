c36e163a008109930914030c83ab551e0961fee9
elnacho02 <nacho.17012002@gmail.com>
2023-02-18 22:59:15 -0300

    feat: first commit

--- projects.ts
+++ projects.ts
@@ -1,38 +1,39 @@
 export type Project = {
-	id: number
-	name: string
-	area: string
-	link: string
-	image: string
-	imageAlt: string
-	color?: string
+  id: number
+  name: string
+  area: string
+  link: string
+  image: string
+  imageAlt: string
+  color?: string
 }
 
 export const projects: Project[] = [
-	{
-		id: 0,
-		name: 'Miinto 2.0',
-		area: 'Design and Development',
-		image: 'https://cdn.dribbble.com/users/1858541/screenshots/17394216/media/113b3233d9f61e804377a2aa2488d613.png',
-		imageAlt: 'Miinto 2.0 by Oliver Cederborg',
-		link: '/project/miinto',
-	},
-	{
-		id: 1,
-		name: 'Tsks.app',
-		area: 'Design and Development',
-		image: 'https://cdn.dribbble.com/users/1858541/screenshots/15209401/media/5781bdac4f41c65f58ea597cad62c2ce.png',
-		imageAlt: 'Tsks task manager app by Oliver Cederborg',
-		link: '/project/tsks',
-		color: '#1D1D26',
-	},
-	{
-		id: 2,
-		name: '100 days of UI',
-		area: 'Design',
-		image: 'https://cdn.dribbble.com/users/1858541/screenshots/15234356/media/5514ddf67d4ba1ab8e763ceb88254253.png',
-		imageAlt: '100 days of UI by Oliver Cederborg',
-		link: '/project/100-days-of-ui',
-		color: '#E3E3E3',
-	},
+  {
+    id: 0,
+    name: 'Glow Skin Care',
+    area: 'UX/UI Case Study',
+    image: 'https://i.ibb.co/mD0GsbX/skin.jpg',
+    imageAlt: 'Glow Skin Care',
+    link: 'https://www.behance.net/gallery/162590201/Glow-UXUI-Case-Study',
+  },
+  {
+    id: 1,
+    name: 'Sierranort Store',
+    area: 'Wireframing Design',
+    image: 'https://i.ibb.co/TKfhKTP/sierr.jpg',
+    imageAlt: 'Sierranort Store',
+    link: 'https://www.behance.net/gallery/163336165/Sierranort',
+    color: '#1D1D26',
+  },
+  {
+    id: 2,
+    name: 'HBO Max Redesign (in process...)',
+    area: 'Design & Research',
+    image:
+      'https://media.a24.com/p/84633a9c1d8ef652962c2fc24920f722/adjuntos/296/imagenes/008/781/0008781720/1200x675/smart/hbo-maxjpg.jpg',
+    imageAlt: 'hbo max',
+    link: '',
+    color: '#E3E3E3',
+  },
 ]
